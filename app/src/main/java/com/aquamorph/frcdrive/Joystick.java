296946a4412fdad0b2f2ec291d8d05dd77dbf126
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-10-05 10:55:22 -0400

    Updated to Gradle based build

---
+++ app/src/main/java/com/aquamorph/frcdrive/Joystick.java
@@ -0,0 +1,123 @@
+package com.aquamorph.frcdrive;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
+public class Joystick extends View implements OnTouchListener{
+
+    interface OnChangeListener{
+        public boolean onChange(byte xAxis, byte yAxis);
+    }
+
+    private byte xAxis = 0;
+    private byte yAxis = 0;
+    private float virtualX = 0;
+    private float virtualY = 0;
+    private float actualX;
+    private float actualY;
+    private Paint inner = new Paint();
+    private Paint outer = new Paint();
+    private int outerRadius;
+    private int innerRadius;
+
+    private OnChangeListener onChange;
+
+    @SuppressLint("ClickableViewAccessibility")
+    public Joystick(Context context, AttributeSet attrs) {
+        super(context,attrs);
+        inner.setARGB(150, 0, 0, 0);
+        outer.setARGB(100, 150, 150, 150);
+        inner.setStyle(Style.FILL);
+        outer.setStyle(Style.FILL);
+        setOnTouchListener(this);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        canvas.drawCircle(outerRadius, outerRadius, outerRadius, outer);
+        canvas.drawCircle(actualX, actualY, innerRadius, inner);
+    }
+
+    public byte getXAxis(){
+        return xAxis;
+    }
+    public byte getYAxis(){
+        return yAxis;
+    }
+
+    public void onSizeChanged(int w, int h, int oldw, int oldh){
+        super.onSizeChanged(w, h, oldw, oldh);
+        outerRadius = w/2;
+        innerRadius = w/4;
+        actualX = outerRadius;
+        actualY = outerRadius;
+    }
+    @SuppressLint("ClickableViewAccessibility")
+    public void setOnChangeListener(OnChangeListener listener){
+        onChange = listener;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouch(View arg0, MotionEvent event) {
+
+        actualX = event.getX();
+        actualY = event.getY();
+        virtualX = actualX - outerRadius;
+        virtualY = outerRadius-actualY;
+        double radius = outerRadius-innerRadius;
+
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:	//Fall through ACTION_MOVE case
+            case MotionEvent.ACTION_MOVE:
+                if(Math.sqrt(Math.pow(virtualX, 2)+Math.pow(virtualY, 2))
+                        >= radius){
+                    double angle= Math.atan(virtualY/virtualX);
+                    if(virtualY > 0 && virtualX > 0){
+                        virtualX = (float) (Math.cos(angle)*radius);
+                        virtualY = (float) (Math.sin(angle)*radius);
+                    }else if(virtualY > 0 && virtualX < 0){
+                        virtualX = (float) -Math.abs(Math.cos(angle)*radius);
+                        virtualY = (float) Math.abs(Math.sin(angle)*radius);
+                    }else if(virtualY < 0 && virtualX < 0){
+                        virtualX = (float) -Math.abs(Math.cos(angle)*radius);
+                        virtualY = (float) -Math.abs(Math.sin(angle)*radius);
+                    }else if(virtualY < 0 && virtualX > 0){
+                        virtualX = (float) (Math.cos(angle)*radius);
+                        virtualY = (float) -Math.abs(Math.sin(angle)*radius);
+                    }else{
+                        virtualX = 0;
+                        virtualY = 0;
+                    }
+                    actualX = virtualX + outerRadius;
+                    actualY = outerRadius-virtualY ;
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+                virtualX = 0;
+                virtualY = 0;
+                actualX = virtualX + outerRadius;
+                actualY = outerRadius-virtualY ;
+                break;
+
+        }
+        invalidate();
+        //Convert joystick range
+        virtualX=(float) ((128/radius)*virtualX);
+        virtualY=(float) ((128/radius)*virtualY);
+        //Convert to byte for packets
+        xAxis = (byte) (virtualX);
+        yAxis = (byte) (virtualY);
+        onChange.onChange(xAxis, yAxis);
+        return true;
+    }
+
+}
---
+++ app/src/main/java/com/aquamorph/frcdrive/MainActivity.java
@@ -0,0 +1,100 @@
+package com.aquamorph.frcdrive;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+    Controls uiManager;
+    PacketSender sender;
+    PhysicalJoystick physicalJoystick;
+    Toolbar toolbar;
+
+    // Menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.wifi:
+                startActivity(new Intent(android.provider.Settings.ACTION_WIFI_SETTINGS));
+                return true;
+            case R.id.tutorial:
+                openTutorial();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void openSettings() {
+        Intent intent = new Intent(this, Preference.class);
+        startActivity(intent);
+    }
+
+    public void openTutorial() {
+        Intent intent = new Intent(this, Tutorial.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.controls);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.wifi:
+                        startActivity(new Intent(android.provider.Settings.ACTION_WIFI_SETTINGS));
+                        return true;
+                    case R.id.tutorial:
+                        openTutorial();
+                        return true;
+                    case R.id.action_settings:
+                        openSettings();
+                        return true;
+                    default:
+                        return true;
+                }
+            }
+        });
+        // Inflate a menu to be displayed in the toolbar
+        toolbar.inflateMenu(R.menu.main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        toolbar.inflateMenu(R.menu.main);
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        uiManager = new Controls(this);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB_MR1) {
+            physicalJoystick = new PhysicalJoystick(this);
+        }
+        sender = new PacketSender(this, uiManager, physicalJoystick);
+        sender.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sender.interrupt();
+        sender = null;
+    }
+
+}
