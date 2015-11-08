6a6ca6d24e73b59fd552db6190c9e2373cdf27a7
Christian Colglazier <aquamorphcomputing@gmail.com>
2015-11-08 11:44:24 -0500

    Updated support for API 23 Updated settings menu to fragment Added protocol version to settings menu

--- app/src/main/java/com/aquamorph/frcdrive/Preference.java
+++ app/src/main/java/com/aquamorph/frcdrive/Preference.java
@@ -6,30 +6,33 @@ import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class Preference extends AppCompatActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.pref_with_actionbar);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(R.string.action_settings);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
 
-        getFragmentManager().beginTransaction().replace(R.id.content_frame, new PrefFragment()).commit();
-    }
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		if(getSupportActionBar() != null) {
+			getSupportActionBar().setTitle(R.string.action_settings);
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+		getFragmentManager().beginTransaction().replace(R.id.content_frame, new PrefFragment()).commit();
+	}
 
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                this.finish();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				this.finish();
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
--- app/src/main/java/com/aquamorph/frcdrive/Tutorial.java
+++ app/src/main/java/com/aquamorph/frcdrive/Tutorial.java
@@ -1,13 +1,32 @@
 package com.aquamorph.frcdrive;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class Tutorial extends Activity {
+public class Tutorial extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.tutorial);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tutorial);
+
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		if(getSupportActionBar() != null) {
+			getSupportActionBar().setTitle(R.string.tutorial);
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				this.finish();
+				break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
